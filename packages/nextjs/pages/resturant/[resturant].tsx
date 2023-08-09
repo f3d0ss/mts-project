285430d7f82c18956420d06d2acaa749d7a2eafa
f3d0ss <federico.cazzola@mail.polimi.it>
2023-08-09 10:12:01 +0200

    feat(nexts): resturant nft sort by date

--- packages/nextjs/pages/resturant/[resturant].tsx
+++ packages/nextjs/pages/resturant/[resturant].tsx
@@ -22,9 +22,11 @@ const ResturantPage: NextPage = dynamic(
             <Spinner height="50px" width="50px" />
           ) : (
             <div className="col-span-5 grid grid-cols-1 lg:grid-cols-5 gap-8 lg:gap-10 p-8">
-              {nfts?.map(nft => (
-                <UserNftPreview nft={nft} key={nft.id.toString()} />
-              ))}
+              {nfts
+                ?.sort((nftA, nftB) => nftA.reservationDate - nftB.reservationDate)
+                .map(nft => (
+                  <UserNftPreview nft={nft} key={nft.id.toString()} />
+                ))}
             </div>
           )}
         </>
