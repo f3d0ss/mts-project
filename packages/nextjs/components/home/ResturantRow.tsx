6f3c3403ece30e11da5ff2230f026f442ef6cc7c
f3d0ss <federico.cazzola@mail.polimi.it>
2023-08-26 23:09:43 +0200

    feat(nextjs): add admin button in resturant rows in controller panel

--- packages/nextjs/components/home/ResturantRow.tsx
+++ packages/nextjs/components/home/ResturantRow.tsx
@@ -26,6 +26,14 @@ export default function ResturantRow({ index, name, address }: ResturantRowProps
         </Link>
       </div>
       <div className="py-5 px-1">
+        <Link
+          className="btn btn-primary rounded-full capitalize font-normal font-white w-24 flex items-center gap-1 hover:gap-2 transition-all tracking-widest"
+          href={`admin/${address}`}
+        >
+          Admin
+        </Link>
+      </div>
+      <div className="py-5 px-1">
         <PauseResturantButton index={index} resturantAddress={address} />
       </div>
       <div className="py-5 px-1">
