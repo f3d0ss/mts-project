3b4b814f03e5ded8d72e91918a19bb4197927809
f3d0ss <federico.cazzola@mail.polimi.it>
2023-08-29 17:38:53 +0200

    refactor(nextjs): refactor useIPFSGateway

--- packages/nextjs/hooks/useIPFSGateway.ts
+++ packages/nextjs/hooks/useIPFSGateway.ts
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { IPFSHTTPClient, create } from "kubo-rpc-client";
+import { useInterval } from "usehooks-ts";
+
+const IPFS_POLLING_INTERVAL = 3000;
 
 export const useIPFSGateway = () => {
   const [ipfsHttpClient, setIpfsHttpClient] = useState<IPFSHTTPClient>();
@@ -27,27 +30,50 @@ export const useIPFSGateway = () => {
     init();
   }, [ipfsHttpClient]);
 
-  const uploadFile = ipfsHttpClient
-    ? async (file: Uint8Array) => {
-        console.log({ isOnline });
-        const result = await ipfsHttpClient.add(file);
-        return result.cid;
-      }
-    : undefined;
+  useInterval(async () => {
+    if (ipfsHttpClient) {
+      try {
+        setIsOnline(await ipfsHttpClient.isOnline());
+      } catch (e) {}
+    }
+  }, IPFS_POLLING_INTERVAL);
 
-  const getFile = ipfsHttpClient
-    ? async (ipfsPath: string) => {
-        if (ipfsPath.startsWith("ipfs://")) {
-          ipfsPath = ipfsPath.substring(7);
-        }
-        const resp = ipfsHttpClient.cat(ipfsPath);
-        let content: Uint8Array = new Uint8Array();
-        for await (const chunk of resp) {
-          content = Uint8Array.from([...content, ...chunk]);
-        }
-        return content;
+  const uploadFile = async (file: Uint8Array) => {
+    try {
+      if (!ipfsHttpClient) throw Error("IPFS not yet initialized");
+      const result = await ipfsHttpClient.add(file);
+      return result.cid;
+    } catch (e) {
+      if (e instanceof Error && (e.message == "Failed to fetch" || e.message == "IPFS not yet initialized")) {
+        console.error("Failed to connect to IPFS");
+        setIsOnline(false);
+        return undefined;
       }
-    : undefined;
+      throw e;
+    }
+  };
+
+  const getFile = async (ipfsPath: string) => {
+    try {
+      if (!ipfsHttpClient) throw Error("IPFS not yet initialized");
+      if (ipfsPath.startsWith("ipfs://")) {
+        ipfsPath = ipfsPath.substring(7);
+      }
+      const resp = ipfsHttpClient.cat(ipfsPath);
+      let content: Uint8Array = new Uint8Array();
+      for await (const chunk of resp) {
+        content = Uint8Array.from([...content, ...chunk]);
+      }
+      return content;
+    } catch (e) {
+      if (e instanceof Error && (e.message == "Failed to fetch" || e.message == "IPFS not yet initialized")) {
+        console.error("Failed to connect to IPFS");
+        setIsOnline(false);
+        return undefined;
+      }
+      throw e;
+    }
+  };
   return {
     isOnline,
     isConnecting,
