a3eb2a5719a64c40800746c6126a15c2f696b3c3
f3d0ss <federico.cazzola@mail.polimi.it>
2023-08-30 22:05:32 +0200

    feat(nextjs): generate ticket show only if not used

--- packages/nextjs/components/my-nft/OwnedNftButton.tsx
+++ packages/nextjs/components/my-nft/OwnedNftButton.tsx
@@ -1,43 +1,15 @@
-import React, { useState } from "react";
-import SignatureResult from "./SignatureResultModal";
-import { encodePacked, keccak256 } from "viem";
-import { useWalletClient } from "wagmi";
+import React from "react";
+import GenerateTicketButton from "./GenerateTicketButton";
 import { ResturantNft } from "~~/types/resturantNft";
-import { notification } from "~~/utils/scaffold-eth";
 
 type OwnedNftButtonProps = {
   nft: ResturantNft;
 };
 
 export default function OwnedNftButton({ nft }: OwnedNftButtonProps) {
-  const [isLoading, setLoading] = useState<boolean>(false);
-
-  //   *When wagmi will add signMessage with raw data
-  //   const { data, isLoading, signMessage: signMessageWagmi } = useSignMessage();
-
-  const wallet = useWalletClient();
-  return (
-    <button
-      className="btn btn-primary rounded-full capitalize font-normal font-white w-24 flex items-center gap-1 hover:gap-2 transition-all tracking-widest"
-      onClick={async () => {
-        const messageHash = keccak256(encodePacked(["uint256", "address"], [nft.id, nft.resturant]));
-        console.log({ messageHash });
-        setLoading(true);
-        const signature = await wallet.data?.signMessage({ message: { raw: messageHash } });
-        setLoading(false);
-        if (signature) {
-          notification.success(<SignatureResult signature={signature} tokenId={nft.id} />, {
-            duration: Infinity,
-          });
-        }
-        //   *When wagmi will add signMessage with raw data
-        //   signMessageWagmi({
-        //     message: messageHash,
-        //   });
-      }}
-      disabled={isLoading}
-    >
-      {isLoading ? "Check Wallet" : "Generate Ticket"}
-    </button>
-  );
+  if (!nft.locked) {
+    return <GenerateTicketButton nft={nft} />;
+  } else {
+    return <p className="font-bold px-4">Used</p>;
+  }
 }
