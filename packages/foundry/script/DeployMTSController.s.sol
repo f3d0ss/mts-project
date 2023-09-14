0029e217542b8647268acc4f8bc75ffe749d6c34
f3d0ss <federico.cazzola@mail.polimi.it>
2023-09-14 17:43:48 +0200

    fix(foudnry): fix deploy for EIP 1167

--- packages/foundry/script/DeployMTSController.s.sol
+++ packages/foundry/script/DeployMTSController.s.sol
@@ -2,12 +2,14 @@
 pragma solidity >=0.8.19 <=0.9.0;
 
 import { MTSController } from "../src/MTSController.sol";
+import { ResturantToken } from "src/ResturantToken.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
 contract DeployMTSController is BaseScript {
     function run(address controllerOwner) public broadcast returns (MTSController) {
-        MTSController mtsController = new MTSController(controllerOwner);
+        ResturantToken resturantTokenImplementation = new ResturantToken();
+        MTSController mtsController = new MTSController(controllerOwner, address(resturantTokenImplementation));
 
         return mtsController;
     }
