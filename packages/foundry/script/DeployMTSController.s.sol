10ff4552421dc6b692e6fb91ce7a72fa39214ba2
f3d0ss <federico.cazzola@mail.polimi.it>
2023-11-02 14:49:10 +0100

    chore(foundry): beautify import

--- packages/foundry/script/DeployMTSController.s.sol
+++ packages/foundry/script/DeployMTSController.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19 <=0.9.0;
 
-import { MTSController } from "../src/MTSController.sol";
+import { MTSController } from "src/MTSController.sol";
 import { ResturantToken } from "src/ResturantToken.sol";
 
 import { BaseScript } from "./Base.s.sol";
